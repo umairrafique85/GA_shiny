4dad3a966ba5ad54ca7b60c7b8faeaf68447dc87
Umair Rafique <umairrafique@marketlytics.com>
2018-11-01 21:23:03 +0500

    Added audit for source/medium

--- server.R
+++ server.R
@@ -1,8 +1,5 @@
 library(shiny)
-library(dplyr)
-library(ggplot2)
-library(readr)
-library(tidyr)
+library(tidyverse)
 library(plotly)
 library(shinydashboard)
 library(googleAnalyticsR)
@@ -10,37 +7,86 @@ select <- dplyr::select
 
 shinyServer(function(input, output, session) {
   
-  observeEvent(input$btn_auth, {
+  observeEvent(input$btn_auth_tab1, {
     ga_auth()
   }) 
   
-  observeEvent(input$btn_accList, {
-    try(output$tableFetched_tab1 <- renderDataTable(ga_account_list()))
+  df_dataPulled <- reactiveVal()
+  self_domain <- reactiveVal()
+  accsList <- reactiveVal()
+  
+  observeEvent(input$btn_accList_tab1, {
+    accsList(ga_account_list())
+    output$dtTbl_accountsList_tab1 <- renderDataTable(accsList())
   })
   
-  observeEvent(input$btn_addDimension, {
-    updateTextInput(session, "txt_dimensions_tab1", 
-                    value = paste(unique(c(unlist(strsplit(input$txt_dimensions_tab1, 
+  observeEvent(input$btn_quickAudit_tab1, {
+    if (str_detect(input$txt_viewId_tab1, "\\S")){
+      try({
+        df_srcMdmData <- google_analytics(input$txt_viewId_tab1, date_range = c(Sys.Date() - 30,
+                                                                                Sys.Date()),
+                                          dimensions = "sourceMedium", metrics = "users")
+      })
+      try({
+        if (input$btn_accList_tab1<1){
+          accsList(ga_account_list())
+        } 
+        rawUrl <- accsList()$websiteUrl[accsList()$viewId==input$txt_viewId_tab1]
+        rawUrl <- unlist(strsplit(rawUrl, split = ".", fixed = TRUE))
+        rawUrl[length(rawUrl)] <- str_extract(rawUrl[length(rawUrl)], "(?:(?!\\/).)*")
+        self_domain(paste(rawUrl[2:length(rawUrl)], collapse = "."))
+      })
+      output$txt_selfDomainTest <- renderText(paste("self domain found in source/Medium:",
+                                                    self_domain() %in% df_srcMdmData$sourceMedium))
+      vct_paymentPortals <- c("pay.amazon.com", "paypal.com", "pay.shopify.com", 
+                              "Infusionsoft.com", "payu.com")
+      output$txt_pmntPortalTest <- renderText(paste("payment portal found in source/Medium:",
+                                                    any(vct_paymentPortals %in% df_srcMdmData$sourceMedium)))
+      df_srcMdmData %>% arrange(desc(users)) %>% head(10) %>% 
+        ggplot(aes(reorder(sourceMedium, users), users, fill = users)) + 
+        geom_bar(stat = 'identity') + scale_fill_gradient(low = 'blue', high = 'red') + 
+        coord_flip() -> srcMdmPlot
+      output$plt_srcMdmTest_tab1 <- renderPlotly(ggplotly(srcMdmPlot))
+    }
+  })
+  
+  observeEvent(input$btn_addDimension_tab2, {
+    updateTextInput(session, "txt_dimensions_tab2", 
+                    value = paste(unique(c(unlist(strsplit(input$txt_dimensions_tab2, 
                                                          split = " ", fixed = T)), 
-                                           input$slct_dimensions_tab1)), 
+                                           input$slct_dimensions_tab2)), 
                                   collapse = " "))
   })
   
-  observeEvent(input$btn_addMetric, {
-    updateTextInput(session, "txt_metrics_tab1", 
-                    value = paste(unique(c(unlist(strsplit(input$txt_metrics_tab1, 
+  observeEvent(input$btn_addMetric_tab2, {
+    updateTextInput(session, "txt_metrics_tab2", 
+                    value = paste(unique(c(unlist(strsplit(input$txt_metrics_tab2, 
                                                            split = " ", fixed = T)), 
-                                           input$slct_metrics_tab1)), 
+                                           input$slct_metrics_tab2)), 
                                   collapse = " "))
   })
   
-  df_dataPulled <- eventReactive(input$btn_fetchData, {
-    try(google_analytics(input$txt_viewId_tab1, date_range = c(input$date_from_tab1, input$date_to_tab1), 
-                     dimensions = unlist(strsplit(input$txt_dimensions_tab1, split = " ")), 
-                     metrics = unlist(strsplit(input$txt_metrics_tab1, split = " "))))
+  dfsInMemory <- reactiveValues()
+  
+  observeEvent(input$btn_fetchData_tab2, {
+    df_dataPulled(try(google_analytics(input$txt_viewId_tab1, date_range = c(input$date_from_tab2, 
+                                                                        input$date_to_tab2), 
+                                  dimensions = unlist(strsplit(input$txt_dimensions_tab2, split = " ")), 
+                                  metrics = unlist(strsplit(input$txt_metrics_tab2, split = " ")))))
   })
   
   observeEvent(df_dataPulled(), {
-    output$tableFetched_tab1 <- renderDataTable(df_dataPulled())
+    output$tableFetched_tab2 <- renderDataTable(df_dataPulled())
+  })
+  
+  observeEvent(input$btn_memorize_tab2, {
+    if (str_detect(input$txt_nameToMemorize_tab2, "\\S")) {
+      dfsInMemory[[input$txt_nameToMemorize_tab2]] <- df_dataPulled()
+      output$tbl_dataInMemory <- renderTable(matrix(names(reactiveValuesToList(dfsInMemory)), 
+                                                    dimnames = list(NULL, "Data frames in memory")))
+    } else {
+      output$txtOut_status_tab2 <- renderText("Please enter name to identify dataframe")
+    }
   })
 })
+  
--- ui.R
+++ ui.R
@@ -2,9 +2,8 @@ library(shiny)
 library(shinydashboard)
 library(DT)
 library(plotly)
-library(RGA)
-
-dimMets <- list_dimsmets()
+library(shinyjs)
+dimMets <- RGA::list_dimsmets()
 avlblDimensions <- subset(dimMets, type=='DIMENSION')$id
 avlblMetrics <- subset(dimMets, type=='METRIC')$id
 avlblDimensions <- matrix(unlist(strsplit(avlblDimensions, split = ":", fixed = T)), ncol = 2, byrow = T)[,2]
@@ -12,63 +11,100 @@ avlblMetrics <- matrix(unlist(strsplit(avlblMetrics, split = ":", fixed = T)), n
 
 dashboardPage(
   dashboardHeader(title = "GA with Shiny"),
+  ## sidebar ####
   dashboardSidebar(
     sidebarMenu(
       menuItem("Pull data", tabName = "tab1_dataFetch"),
       menuItem("Wrangle", tabName = "tab2_wrangle")
-    )
+    ),
+    tableOutput("tbl_dataInMemory")
   ),
   dashboardBody(
     tabItems(
+      ## Data fetch tab and audit tab ####
       tabItem(tabName = "tab1_dataFetch",
-        h2("Authenticate and Fetch data"),
-        fluidRow(      
-          actionButton("btn_auth", "Authenticate"),
-          actionButton("btn_accList", "Fetch Accounts List")
+        h2("Authenticate, Fetch accounts list, and quick audit"),
+        fluidRow( ## Top row with buttons ####     
+          actionButton("btn_auth_tab1", "Authenticate"),
+          actionButton("btn_accList_tab1", "Fetch Accounts List")
         ),
-        fluidRow(
+        fluidRow( ## View Id ####
           column(width = 4,  
             textInput("txt_viewId_tab1", "View Id:")
           ),
           column(width = 4,
-            dateInput("date_from_tab1", "From:", value = Sys.Date()-1)
+            actionButton("btn_quickAudit_tab1", "Quick Audit")
           ),
-          column(width = 4,
-            dateInput("date_to_tab1", "To:", value = Sys.Date()-1)
+          column(width = 4
           )
         ),
-        fluidRow(
-          column(width = 6, 
-            textInput("txt_dimensions_tab1", label = "Dimensions selected:")
-          ),
-          column(width = 6,
-            textInput("txt_metrics_tab1", label = "Metrics selected:")
-          )
+        fluidRow( ## accounts list
+          box(dataTableOutput("dtTbl_accountsList_tab1"))        
         ),
-        fluidRow(
-          column(width = 5,
-            selectInput("slct_dimensions_tab1", "Select dimension to add", choices = avlblDimensions)
-          ),
-          column(width = 1,
-            actionButton("btn_addDimension", "+")
-          ),
-          column(width = 5,
-            selectInput("slct_metrics_tab1", "Select metric to add", choices = avlblMetrics)
-          ),
-          column(width = 1,
-            actionButton("btn_addMetric", "+")
-          )
-        ),
-        fluidRow(
-          actionButton("btn_fetchData", "Fetch Data")
-        ),
-        fluidRow(
-          box(dataTableOutput("tableFetched_tab1"))
+        fluidRow( ## audit info ####
+          box(width = 6,
+            plotlyOutput("plt_srcMdmTest_tab1"),
+            textOutput("txt_selfDomainTest"),
+            textOutput("txt_pmntPortalTest")
+          )        
         )
       ),
-      tabItem(tabName = "tab2_wrangle"
-
+      ## Data fetch and wrangle tab ####
+      tabItem(tabName = "tab2_wrangle",
+        fluidRow(
+          column(width = 8
+          ),
+          column(width = 2,
+            dateInput("date_from_tab2", "From:", value = Sys.Date()-1)
+          ),
+          column(width = 2,
+            dateInput("date_to_tab2", "To:", value = Sys.Date()-1)
+          )
+        ),
+        fluidRow( ## Dimensions and metrics textboxes ####
+          column(width = 6, 
+            textInput("txt_dimensions_tab2", label = "Dimensions selected:")
+          ),
+          column(width = 6,
+            textInput("txt_metrics_tab2", label = "Metrics selected:")
+          )
+        ),
+        fluidRow( ## Dimensions and metrics selectors ####
+          column(width = 5,
+            selectInput("slct_dimensions_tab2", "Select dimension to add", choices = avlblDimensions)
+          ),
+          column(width = 1,
+            actionButton("btn_addDimension_tab2", "+")
+          ),
+          column(width = 5,
+            selectInput("slct_metrics_tab2", "Select metric to add", choices = avlblMetrics)
+          ),
+          column(width = 1,
+            actionButton("btn_addMetric_tab2", "+")
+          )
+        ),
+        fluidRow( ## Fetch data and display, and memorize options ####
+          column(width = 1,
+            actionButton("btn_fetchData_tab2", "Fetch Data")
+          ),
+          column(width = 3,
+            tags$b("Memorize by name:")
+          ),
+          column(width = 4,
+            textInput("txt_nameToMemorize_tab2", NULL)     
+          ),
+          column(width = 1,
+            actionButton("btn_memorize_tab2", "Memorize")     
+          ),
+          column(width = 3,
+            textOutput("txtOut_status_tab2")     
+          )
+        ),
+        fluidRow(
+          box(dataTableOutput("tableFetched_tab2"))
+        )
       )
+    ## end of tabItems ####
     )
   )
 )
